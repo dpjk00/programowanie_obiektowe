42fc93f9b0598db2ce0c3ace871baadd47da6ea0
dpjk00 <91792912+dpjk00@users.noreply.github.com>
2022-04-02 19:15:30 +0200

    lab3com1
    lab3

---
+++ lab03/FileLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+System.IO
+
+namespace lab03
+{
+    class FileLogger : ILogger
+    {
+
+        FileStream stream = new FileStream("log.txt", FileMode.Append);
+        writer = new StreamWriter(stream);
+        public void Dispose()
+        {
+
+        }
+    }
+}
